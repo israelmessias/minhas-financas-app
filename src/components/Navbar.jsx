2aac8c5dc86a08ed1b2b459507ffbc35dc39c62e
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-11 18:07:24 -0300

    Titulo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ function NavBar ()
   return(
     <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary" >
       <div className="container">
-        <a href="https://bootswatch.com/" className="navbar-brand">Minhas Finanças</a>
+        <a href="https://bootswatch.com/" className="navbar-brand">MINHAS FINANÇAS</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav">
-            <NavBarIntem href="#/" label="Home"/>
+            <NavBarIntem href="#/home" label="Home"/>
             <NavBarIntem href="#/cadastro-user" label="Usuários"/>
             <NavBarIntem href="#" label="Lançamentos" />
             <NavBarIntem href="#/login" label="Login" />
