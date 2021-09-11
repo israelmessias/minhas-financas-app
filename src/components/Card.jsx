55603130a870e0aab908b1ed4f8357dc82c01046
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-11 08:48:56 -0300

    card login

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-class Card extends React.Component{
+class Card extends React.Component
+{
     /*card md-3 class mãe
     card-header o cabeçalho Login
     card-body corpo do card*/
-    render(){
+    render()
+    {
         return (
         <div className="card md-3">
             <h3 className="card-header">{this.props.title}</h3>
