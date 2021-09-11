bedeb8416d5866e8e672a4fc806000551dd7bd53
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-11 15:16:12 -0300

    Rotas para os botões de nave

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import NavBarIntem from "./NavbarIntem";
+
 function NavBar ()
 {
   return(
@@ -11,19 +13,11 @@ function NavBar ()
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav">
-            <li className="nav-item">
-            <a className="nav-link" href="home.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="usuarios.html">Usuários</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="lancamentos.html">Lançamentos</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="login.html">Login</a>
-          </li>
-        </ul>
+            <NavBarIntem href="#/" label="Home"/>
+            <NavBarIntem href="#/cadastro-user" label="Usuários"/>
+            <NavBarIntem href="#" label="Lançamentos" />
+            <NavBarIntem href="#/login" label="Login" />
+        </ul> 
 
         </div>
       </div>
