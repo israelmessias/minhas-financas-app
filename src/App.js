ba22d9bef51dd3295906e449a3e5ddc546e53833
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-11 08:48:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Login from '../src/views/Login'
 import 'bootswatch/dist/pulse/bootstrap.css'
+import './views/custom.css'
 
 class App extends React.Component{
-  render(){
+  render()
+  {
     return(
     <div>
       <Login />
