c2d02031a70a093157a84ac3da55ef3562b719f8
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-11 08:48:28 -0300

    Login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,7 +3,21 @@ import Card from '../components/Card'
 import FormGroup from "../components/Form-group";
 
 class Login extends React.Component{
-    render(){
+
+    state =
+    {
+        email: '',
+        senha: '',
+    }
+
+    entrar = () =>
+    {
+        console.log('Email', this.state.email)
+        console.log('senha', this.state.senha)
+    }
+
+    render()
+    {
         return(
             //Container centraliza o conteudo
             <div className="container"> 
@@ -16,19 +30,26 @@ class Login extends React.Component{
                                         <div className="bs-component">
                                             <fieldset>
                                                 <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                <input type="email" 
-                                                className="form-control"
-                                                 id="exampleInputEmail1"
-                                                aria-describedby="emailHelp"
-                                                 placeholder="Digite o Email" />
+                                                    <input type="email" 
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({email: e.target.value})}
+                                                    className="form-control"
+                                                    id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Digite o Email" />
                                                 </FormGroup>
 
                                                 <FormGroup label="Senha: *" htmlFor="">
-                                                 <input type="password"
-                                                  className="form-control"
-                                                   id="exampleInputPassword1" 
-                                                   placeholder="Password" />
+                                                    <input type="password"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({senha: e.target.value})}
+                                                    className="form-control"
+                                                    id="exampleInputPassword1" 
+                                                    placeholder="Password" />
                                                 </FormGroup>
+
+                                                <button onClick={ this.entrar() } className="btn btn-success">Entrar</button>
+                                                <button className="btn btn-danger">Cadastrar</button>
                                             </fieldset>
                                         </div>
                                     </div>
