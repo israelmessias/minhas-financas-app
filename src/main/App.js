ca555857faf455a00dc1800b9387ba83567042d4
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-16 20:17:37 -0300

    css do toastr

--- src/main/App.js
+++ src/main/App.js
@@ -4,6 +4,8 @@ import Rotas from './Rotas'
 import Navbar from '../components/Navbar';
 import 'bootswatch/dist/pulse/bootstrap.css'
 import '../views/custom.css'
+import 'toastr/build/toastr.css'
+import 'toastr/build/toastr.min.js'
 
 class App extends React.Component{
   render()
