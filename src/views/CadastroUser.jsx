aecbe3478be45521bea68db89c63f34a9f1aa324
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-16 21:32:53 -0300

    validações

--- src/views/CadastroUser.jsx
+++ src/views/CadastroUser.jsx
@@ -23,6 +23,16 @@ class CadastroUser extends React.Component
 
     cadastrar = () =>
     {
+        const msgs = this.validar();
+
+        if(msgs && msgs.length > 0)
+        {
+            msgs.forEach(msgs, index => {
+                mostrarErro(msgs)
+            })
+            return false;
+        }
+
         const usuario = {
             nome: this.state.nome,  
             email: this.state.email,
@@ -35,6 +45,30 @@ class CadastroUser extends React.Component
             }).catch(erro =>{
                 mostrarErro(erro.response.data)
             })
+    }
+
+    validar()
+    {
+        const msgs = []
+
+        if(!this.state.nome)
+        {
+            msgs.push('O campo Nome é obrigatorio!')
+        }
+        if(!this.state.email)
+        {
+            msgs.push('O campo Email é obrigatorio!')
+        }
+        if(!this.state.senha || !this.state.senhaRepeticao)
+        {
+            msgs.push('Digite a senha duas vezes')
+        }else if(this.state.senha !== this.state.senhaRepeticao)
+        {
+            msgs.push('As senhas não batem.')
+        }
+
+
+        return msgs;
     }
 
     render()
