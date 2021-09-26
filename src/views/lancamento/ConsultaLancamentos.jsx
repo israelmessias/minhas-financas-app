24de183eeb3442f252975ae7bbb942b2e1cde504
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-25 21:40:29 -0300

    implementando :-1:

--- src/views/lancamento/ConsultaLancamentos.jsx
+++ src/views/lancamento/ConsultaLancamentos.jsx
@@ -3,26 +3,48 @@ import { withRouter } from 'react-router-dom'
 import Card from '../../components/Card'
 import FormGroup from '../../components/Form-group';
 import SelectMenu from '../../components/SelectMenu'
+import LancamentoTable from './LancamentoTable'
 
 
 class ConsultaLancamentos extends React.Component
 {
     render()
     {
-        const lista = [
-            {label: 'SELECIONE ...', value: ''},
+
+        const state= {
+            ano: '',
+            mes: '',
+            tipo: ''
+        }
+
+        /*buscar = () =>
+        {
+            console.log(this.state)
+        }*/
+
+        const meses = [
+            {label: 'Selecione...', value: ''},
             {label: 'Janeiro', value: 1},
             {label: 'Fevereiro', value: 2},
             {label: 'Março', value: 3},
             {label: 'Abril', value: 4},
             {label: 'Maio', value: 5},
-            {label: 'Junho', value: 6},
             {label: 'Julho', value: 7},
+            {label: 'Junho', value: 6},
             {label: 'Agosto', value: 8},
             {label: 'Setembro', value: 9},
             {label: 'Outubro', value: 10},
             {label: 'Novembro', value: 11},
             {label: 'Dezembro', value: 12},
+        ]
+
+        const tipos = [
+            {label: 'Selecione...', value: ''},
+            {label: 'Despesa', value: 'DESPESA'},
+            {label: 'Receita', value: 'RECEITA'},
+        ]
+        const lancamentos = [
+            {id:1, descricao: 'boleto', valor: 5,  mes: 1, tipo:'Despesa', status: 'Efetivado'}
         ]
 
         return(
@@ -32,12 +54,37 @@ class ConsultaLancamentos extends React.Component
                         <div className="bs-component">
                             <FormGroup htmlFor="inputAno" label="Ano: *">
                             <input type="text" className="form-control" id="inputAno" 
-                            aria-describedby="emailHelp" placeholder="Digite o Ano"/>
+                            value={this.state.ano}
+                            onChange={e =>this.setState({ano: e.target.value})} 
+                            placeholder="Digite o Ano"/>
                             </FormGroup>
 
                             <FormGroup htmlFor="inputMes" label="Mês: *">
-                                <SelectMenu  lista={lista}/>
+                                <SelectMenu id="inputMes" className="form-control"
+                                value={this.state.mes}
+                                onChange={e =>this.setState({mes: e.target.value})}
+                                 lista={meses}/>
                             </FormGroup>
+
+                            <FormGroup htmlFor="inputTipo" label="Tipo: *">
+                                <SelectMenu id="inputTipo" className="form-control"
+                                value={this.state.tipo}
+                                onChange={e =>this.setState({tipo: e.target.value})}
+                                 lista={tipos}/>
+                            </FormGroup>
+
+                            <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
+                            <button type="button" className="btn btn-danger">Cadastrar</button>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="bs-component">
+                            <LancamentoTable lancamentos={lancamentos} />
                         </div>
                     </div>
                 </div>
