3f8d2de1b6337c6d074779872fc2c4f0c401374c
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-10-03 12:00:17 -0300

    Tema do primereact

--- src/main/App.js
+++ src/main/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import Rotas from './Rotas'
 import Navbar from '../components/Navbar';
+
+
 import 'bootswatch/dist/pulse/bootstrap.css'
 import '../views/custom.css'
+
 import 'toastr/build/toastr.css'
 import 'toastr/build/toastr.min.js'
 
-
-
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/md-light-deeppurple/theme.css'
+import 'primeicons/primeicons.css'
 
 class App extends React.Component{
   render()
