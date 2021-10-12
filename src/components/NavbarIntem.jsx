6206b260e38eaac6419b1c108a7eaa03cd525d43
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-10-12 19:01:37 -0300

    Update NavbarIntem.jsx

--- src/components/NavbarIntem.jsx
+++ src/components/NavbarIntem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavBarIntem(props){
     return(
         <li className="nav-item">
-            <a className="nav-link" href={props.href}>{props.label}</a>
+            <a onClick={props.onClick} className="nav-link" href={props.href}>{props.label}</a>
         </li>
     )
 }
