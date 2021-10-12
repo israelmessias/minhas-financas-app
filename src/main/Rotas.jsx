5f19e81857ada0b4c0d1ca9334f95cbde713a909
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-10-12 18:08:42 -0300

    Autenticação nas rotas

--- src/main/Rotas.jsx
+++ src/main/Rotas.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 
-import {Route, Switch, HashRouter } from 'react-router-dom'
+import {Route, Switch, HashRouter, Redirect } from 'react-router-dom'
 
 import Login from '../views/Login'
 import CadastroUser from '../views/CadastroUser'
 import Home from '../views/Home'
 import ConsultaLancamentos from "../views/lancamento/ConsultaLancamentos";
 import CadastroLancamento from "../views/lancamento/CadastroLancamento";
+
+
+const UsuarioAutenticado = () =>
+{
+    return false;
+}
+
+/*Sempre que for tenta entrar em uma rota sem ter autenticado
+* Sera sempre redirecionado para o login*/
+const RotasAutenticadas = ({component: Componet, ...props}) =>
+{
+    return(
+        <Route {...props} render={ (componentProps) => {
+           if(UsuarioAutenticado()) 
+           {
+                return (
+                    <Componet {...componentProps}/>
+            )
+           }else{
+               return(
+                    <Redirect to={ { pathname: '/login', state: { from: componentProps.location } } } />
+                    )
+           }   
+        }} />
+    )
+}
 
 /*
 *   HashRouter -> /#/ vai buscar na pasta raiz
@@ -19,11 +45,11 @@ function Rotas()
     return (
         <HashRouter>
             <Switch>
-                <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-user" component={CadastroUser} />
-                <Route path="/consulta-lancamento" component={ConsultaLancamentos} />
-                <Route path='/cadastro-lancamento/:id?' component={CadastroLancamento} />
+                <RotasAutenticadas path="/home" component={Home} />
+                <RotasAutenticadas path="/consulta-lancamento" component={ConsultaLancamentos} />
+                <RotasAutenticadas path='/cadastro-lancamento/:id?' component={CadastroLancamento} />
             </Switch>
         </HashRouter>
         
