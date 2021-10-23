5860f56b4d5ac8eab8907dc6ab44134e7fdd9466
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-10-23 15:17:11 -0300

    Adicionado url da api remota

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const httpClient = axios.create
 ({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://minhasfinancasapi-lawmodel.herokuapp.com'
 })
 
 class ApiService{
